8a515d32d2f6cc5bef3e1f02c8569d4ddb2b7774
ShanJiang1911 <johnson33399@gmail.com>
2021-12-02 23:48:27 -0800

    add lab04

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,38 @@
+class Band():
+    def __init__(self, name):
+        self.name = name
+        
+
+class Musician():
+    pass
+
+class Guitarist():
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+
+class Bassist():
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+class Drummer():
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
